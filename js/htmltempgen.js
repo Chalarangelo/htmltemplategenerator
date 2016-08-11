99aacfd3136959174bbee586c241d56ef635a826
Angelos Chalaris <chalarangelo@gmail.com>
2016-08-11 12:52:01 +0300

    Interface naming conventions changed to better reflect the scope of the code, code implementation for interface parts and user-defined body

--- js/htmltempgen.js
+++ js/htmltempgen.js
@@ -1,7 +1,7 @@
 $(function(){
 	var debug = true;
 	// Version name and setup, always use this and update accordingly
-	var versionName = 'v1.0.0_pre09 (cuttlefish_production'+((debug)?'::debug_true':'')+')';
+	var versionName = 'v1.0.0_pre11 (cuttlefish_production'+((debug)?'::debug_true':'')+')';
 	// Create the templateGen for use in the rest of the application
 	var result = new templateProto();
 	$('.versionNumbering').text(versionName);
@@ -13,19 +13,20 @@ $(function(){
 		$(this).addClass('active');
 		$('.activeRow').removeClass('activeRow').addClass('hidden');
 		$('#'+$(this).attr('id').substr(0, $(this).attr('id').indexOf('-'))).removeClass('hidden').addClass('activeRow');
-		if($(this).attr('id')=='result-tab'){ $('#result-copier').val(result.toText()); editor.setValue(result.toText(), -1); }
-			
+		if($(this).attr('id')=='result-tab'){ editor.setValue(result.toText(), -1); }			
 	});
 	// Content tab events
 	$('#content-title').change(function(){	result.head.title = $('#content-title').val();	});
 	$('#content-classes').change(function(){	result.body.classes = $('#content-classes').val();	});
 	$('#content-body').change(function(){	result.body.userBody = $('#content-body').val();	});
-	$('input[name="content-pos-selector"]').change(function(){	result.body.before = $('#con-before:checked').length>0;	});
+	$('input[name="content-pos-selector"]').change(function(){	result.body.before = $('#con-before:checked').length>0;	console.log(result.body.before);});
 	$('select[name="content-templates"]').change(function(){
-		var selected = $('select[name="content-templates"] option:selected' ).val();
+		var selected = $('select[name="content-templates"] option:selected' ).val();	result.body.templateBase = selected;
 		$('.templates-customizer').addClass('hidden');	$('#content-'+selected+'-customize').removeClass('hidden');
+		result.body.templateId = $('[name="content-'+selected+'-selector"]:checked').attr('id');
 	});
 	$('#commenter').change(function(){result.comments = $('#commenter:checked').length>0;});
+	$('input[name^="content-"][name$="-template-selector"]').change(function(){ result.body.templateId = $(this).attr('id'); });
 	// Libraries tab events
 	$('#lib-loader input:checkbox').change(function(){$('#libraries-badge').text($('#lib-loader input:checkbox:checked').length);});
 	// Metadata tab events
@@ -83,9 +84,9 @@ var templateProto = function(){
 	this.body = {
 		classes: '', 
 		before: true, 
-		base: 0, 
 		userBody: '', 
-		templateBody: ''
+		templateBase: 'page-template',
+		templateId: 'pt-empty'
 	};
 	this.toText = function(){
 		var textRes = this.doctype + '\n' + this.html.start +'\n'
@@ -106,7 +107,7 @@ var templateProto = function(){
 	}
 	this.metaToText = function(){
 		var metaText = '';
-		metaText += (this.comments?'    <!-- Start of Metadata -->\n':'');
+		metaText += (this.comments?'    <!-- Start of metadata -->\n':'');
 		metaText += ((this.head.meta.charset == 0)?'    <meta charset="utf-8">':'    <meta charset="iso-8859-1">')+'\n';
 		metaText += ((this.head.meta.viewport)?'    <meta name="viewport" content="width=device-width, initial-scale=1">\n':'');
 		metaText += (($.trim(this.head.meta.description).length === 0)?'':'    <meta name="description" content="'+$.trim(this.head.meta.description)+'">\n');
@@ -115,28 +116,47 @@ var templateProto = function(){
 		metaText += ((this.head.meta.gentag)?'    <meta name="generator" content="http://chalarangelo.github.io/htmltemplategenerator/">\n':'');
 		metaText += ((this.head.meta.favicon)?'    <link rel="icon" type="image/x-icon" href="./favicon.ico">\n':'');
 		metaText += ((this.head.meta.oldwarn)?'    <!--[if lt IE 8]>\n      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>\n    <![endif]-->\n':'');
-		metaText += (this.comments?'    <!-- End of Metadata -->\n':'');
+		metaText += (this.comments?'    <!-- End of metadata -->\n':'');
 		return metaText;
 	}
 	this.librariesToText = function(){
 		var libText = '';
-		libText += (this.comments?'    <!-- Start of Libraries -->\n':'');
+		libText += (this.comments?'    <!-- Start of libraries -->\n':'');
 		// TODO: Add libraries code here!
-		libText += (this.comments?'    <!-- End of Libraries -->\n':'');
+		libText += (this.comments?'    <!-- End of libraries -->\n':'');
 		return libText;
 	}
 	this.resourcesToText = function(){
 		var resText ='';
-		resText += (this.comments?'    <!-- Start of Resources -->\n':'');
+		resText += (this.comments?'    <!-- Start of resources -->\n':'');
 		// TODO: Add resources code here!
-		resText += (this.comments?'    <!-- End of Resources -->\n':'');
+		resText += (this.comments?'    <!-- End of resources -->\n':'');
 		return resText;
 	}
 	this.bodyToText = function(){
 		var bodyText = '';
 		bodyText += (($.trim(this.body.classes).length === 0)?(this.html.body.start+'\n'):[this.html.body.start.slice(0, 7), ' classes="'+$.trim(this.body.classes)+'"',this.html.body.start.slice(7)].join('')+'\n');
-		bodyText += this.html.body.end + '\n';
+		bodyText += ((this.body.before)?(this.userBodyToText()+this.templateBodyToText()):(this.templateBodyToText()+this.userBodyToText()));
+		bodyText += this.html.body.end + '\n';		
 		return bodyText;
+	}
+	this.userBodyToText = function(){
+		var bodyUserText = '';
+		bodyUserText += (this.comments?'    <!-- Start of user-defined body -->\n':'');
+		var bodyUserLines = this.body.userBody.match(/[^\r\n]+/g);
+		if(bodyUserLines !== null)
+			for(var i = 0; i <bodyUserLines.length; i ++)
+				bodyUserText+='    '+bodyUserLines[i]+'\n';
+		bodyUserText += (this.comments?'    <!-- End of user-defined body -->\n':'');
+		return bodyUserText;
+	}
+	this.templateBodyToText = function(){
+		var bodyTemplateText = '';
+		bodyTemplateText += (this.comments?'    <!-- Start of generated template -->\n':'');
+		//switch()
+		// TODO: Add tempate switch and code here!
+		bodyTemplateText += (this.comments?'    <!-- End of generated template -->\n':'');
+		return bodyTemplateText;
 	}
 };
 /*===============OLD CODE====================================================
